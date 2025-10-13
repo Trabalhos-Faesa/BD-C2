8b83bb7880f5fd370c218426c026ca576fc46974
Elias De Nadai Nalesso Silva <elias.silva@globalsys.com.br>
2025-10-13 05:23:39 -0300

    feat: add views.cliente.create

--- src/views/cliente.py
+++ src/views/cliente.py
@@ -1,12 +1,21 @@
 from fastapi import APIRouter
 
 from abstract import SQLResult, SQLResultDict
+from models.cliente import Cliente
 from utils.db import exec_query
 
 
 router = APIRouter(prefix='/cliente', tags=['cliente'])
 
 
-@router.get('/', response_model=SQLResult)
+@router.get('/', response_model=SQLResult[Cliente])
 async def read_all() -> SQLResultDict:
     return exec_query('cliente/read_all.sql')
+
+
+@router.post('/', response_model=SQLResult[Cliente])
+async def create(cliente: Cliente) -> SQLResultDict:
+    return exec_query(
+        'cliente/create.sql',
+        cliente.model_dump()
+    )
