e6a1fa6ef3ab356c00495c97dffbe0dbc21a988e
Elias De Nadai Nalesso Silva <elias.silva@globalsys.com.br>
2025-10-13 00:04:41 -0300

    style: add the '.' chars

--- src/utils/validators.py
+++ src/utils/validators.py
@@ -23,24 +23,24 @@ def valid_cpf(cpf: str) -> str:
     cpf_digits = [int(digit) for digit in cpf if digit.isdecimal()]
 
     if len(cpf_digits) != 11:
-        raise ValueError(f'CPF "{cpf}" inválido pois não contém 11 caracteres numéricos')
+        raise ValueError(f'CPF "{cpf}" inválido pois não contém 11 caracteres numéricos.')
 
     # # Verifica a formatação do CPF
     # if not re.match(r'\d{3}\.\d{3}\.\d{3}-\d{2}', cpf):
     #     return False
     if len(cpf_digits) != len(cpf):
-        raise ValueError(f'CPF "{cpf}" inválido pois contém caracteres não numéricos')
+        raise ValueError(f'CPF "{cpf}" inválido pois contém caracteres não numéricos.')
 
     # Validação do primeiro dígito verificador:
     sum_of_products = sum(a*b for a, b in zip(cpf_digits[0:9], range(10, 1, -1)))
     expected_digit = (sum_of_products * 10 % 11) % 10
     if cpf_digits[9] != expected_digit:
-        raise ValueError(f'O primeiro dígito verificador do CPF "{cpf}" é inválido')
+        raise ValueError(f'O primeiro dígito verificador do CPF "{cpf}" é inválido.')
 
     # Validação do segundo dígito verificador:
     sum_of_products = sum(a*b for a, b in zip(cpf_digits[0:10], range(11, 1, -1)))
     expected_digit = (sum_of_products * 10 % 11) % 10
     if cpf_digits[10] != expected_digit:
-        raise ValueError(f'O segundo dígito verificador do CPF "{cpf}" é inválido')
+        raise ValueError(f'O segundo dígito verificador do CPF "{cpf}" é inválido.')
 
     return ''.join([str(d) for d in cpf_digits])
