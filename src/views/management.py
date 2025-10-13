f9bcce5ed9e232ddb181557fbe97bff0f6ace0c3
Elias De Nadai Nalesso Silva <elias.silva@globalsys.com.br>
2025-10-13 03:39:17 -0300

    feat: add views.management

--- src/views/management.py
+++ src/views/management.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 
+from abstract import SQLResult
+from utils.db import exec_query
+
 
 router = APIRouter(prefix='/-', tags=['management'])
 
@@ -7,3 +10,8 @@ router = APIRouter(prefix='/-', tags=['management'])
 @router.get('/health')
 async def health_check():
     return {'status': 'healthy'}
+
+
+@router.post('/reset_db', response_model=SQLResult)
+async def reset_db():
+    return exec_query('management/reset_db.sql')
