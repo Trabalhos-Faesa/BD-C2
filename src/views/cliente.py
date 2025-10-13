4b1d7cc40872d181e965628da91ecb000a62e038
Elias De Nadai Nalesso Silva <elias.silva@globalsys.com.br>
2025-10-13 06:16:35 -0300

    feat: use aexec_query

--- src/views/cliente.py
+++ src/views/cliente.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from abstract import SQLResult, SQLResultDict
 from models.cliente import Cliente
-from utils.db import exec_query
+from utils.db import aexec_query
 
 
 router = APIRouter(prefix='/cliente', tags=['cliente'])
@@ -10,12 +10,12 @@ router = APIRouter(prefix='/cliente', tags=['cliente'])
 
 @router.get('/', response_model=SQLResult[Cliente])
 async def read_all() -> SQLResultDict:
-    return exec_query('cliente/read_all.sql')
+    return await aexec_query('cliente/read_all.sql')
 
 
 @router.post('/', response_model=SQLResult[Cliente])
 async def create(cliente: Cliente) -> SQLResultDict:
-    return exec_query(
+    return await aexec_query(
         'cliente/create.sql',
         cliente.model_dump()
     )
