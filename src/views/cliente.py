71d9037e9b7f4b72b495d5e18c293af13031670e
Elias De Nadai Nalesso Silva <elias.silva@globalsys.com.br>
2025-10-20 03:07:27 -0300

    minor refactor

--- src/views/cliente.py
+++ src/views/cliente.py
@@ -8,6 +8,14 @@ from utils.db import aexec_query
 router = APIRouter(prefix='/cliente', tags=['cliente'])
 
 
+@router.post('/', response_model=SQLResult[Cliente])
+async def create(cliente: Cliente) -> SQLResultDict:
+    return await aexec_query(
+        'cliente/create.sql',
+        cliente.model_dump(),
+    )
+
+
 @router.get('/', response_model=SQLResult[Cliente])
 async def read_all() -> SQLResultDict:
     return await aexec_query('cliente/read_all.sql')
@@ -19,13 +27,5 @@ async def read_one(id_cliente: int) -> SQLResultDict:
         'cliente/read_one.sql',
         {
             'id_cliente': id_cliente,
-        }
-    )
-
-
-@router.post('/', response_model=SQLResult[Cliente])
-async def create(cliente: Cliente) -> SQLResultDict:
-    return await aexec_query(
-        'cliente/create.sql',
-        cliente.model_dump()
+        },
     )
