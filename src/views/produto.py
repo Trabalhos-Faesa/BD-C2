c286cd8bb7f648f2f0fd4e910550eff4de8400e3
João Pedro Rosa Da Silva <joaopedrors080@gmail.com>
2025-10-20 21:52:43 -0300

    adicionar views relativas a produto

---
+++ src/views/produto.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter
+
+from abstract import SQLResult, SQLResultDict
+from models.produto import Produto
+from utils.db import aexec_query
+
+
+router = APIRouter(prefix='/produto', tags=['produto'])
+
+
+@router.post('/', response_model=SQLResult[Produto])
+async def create(produto: Produto) -> SQLResultDict:
+    return await aexec_query(
+        'produto/create.sql',
+        produto.model_dump(),
+    )
+
+
+@router.get('/', response_model=SQLResult[Produto])
+async def read_all() -> SQLResultDict:
+    return await aexec_query('produto/read_all.sql')
+
+
+@router.get('/{id_produto}', response_model=SQLResult[Produto])
+async def read_one(id_produto: int) -> SQLResultDict:
+    return await aexec_query(
+        'produto/read_one.sql',
+        {
+            'id_produto': id_produto,
+        },
+    )
+
+
+# TODO: Partial update with .patch
+@router.put('/{id_produto}', response_model=SQLResult[Produto])
+async def update(id_produto: int, produto: Produto) -> SQLResultDict:
+    return await aexec_query(
+        'produto/update.sql',
+        produto.model_dump() | {
+            'id_produto': id_produto,
+        },
+    )
+
+
+@router.delete('/{id_produto}', response_model=SQLResult[Produto])
+async def delete(id_produto: int) -> SQLResultDict:
+    return await aexec_query(
+        'produto/delete.sql',
+        {
+            'id_produto': id_produto,
+        }
+    )
+
